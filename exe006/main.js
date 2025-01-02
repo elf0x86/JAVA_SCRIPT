3488a960e9ecadd7d4476104d4447f88bf8cb588
elf0x86 <samcodebeliver@gmail.com>
2025-01-02 15:20:09 -0300

    Update main.js

--- exe006/main.js
+++ exe006/main.js
@@ -1,1 +1,8 @@
 
+function process() {
+	let valor = parseInt(document.querySelector('#userData').value);
+
+	document.querySelector('#dobro').innerText = `O dobro ${valor * 2}`;
+	document.querySelector('#triplo').innerText = `O triplo ${valor * 3}`;
+	document.querySelector('#raizQuadrada').innerText = `A raiz ${valor ** (1/2)}`;
+}
